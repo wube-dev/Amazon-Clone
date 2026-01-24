ad8a64b2efdf7f4a86d290c440dafb7f1b1134f4
wube_dev <eliyasawraris922@gmail.com>
2026-01-24 22:51:39 +0300

    Fix all import casing issues and ensure build passes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,13 @@
+// import axios from "axios";
+// const axiosInstance = axios.create({
+//   // baseURL: "http://127.0.0.1:5001/e-clone-6c5eb/us-central1/api",
+//   baseURL: "https://amazon-clone-backend-1-b7na.onrender.com",
+// });
+// export { axiosInstance };
+  
 import axios from "axios";
-const axiosInstance = axios.create({
-  // baseURL: "http://127.0.0.1:5001/e-clone-6c5eb/us-central1/api",
+
+export const axiosInstance = axios.create({
   baseURL: "https://amazon-clone-backend-1-b7na.onrender.com",
+  headers: { "Content-Type": "application/json" },
 });
-export { axiosInstance };
